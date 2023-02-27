83d9ea5be6650069302d9d8a1980147f6465eaed
alvaro@honeypot <alvaro@honeypot>
2023-02-27 16:15:00 +0100

    errores de doble liberación de malloc free

--- libnss_honeypot.c
+++ libnss_honeypot.c
@@ -88,12 +88,51 @@ enum nss_status _nss_honeypot_getpwuid_r(uid_t uid, struct passwd *result, char 
         return NSS_STATUS_NOTFOUND;
 }
 
+int append_to_buffer(const char* str, char **buffer, size_t *buflen )
+{
+  int len = 0;
+  
+  if( buflen <= 0 ){
+    return -1;
+  }
+
+  strncpy(*buffer, str, *buflen-1 );
+  len = strnlen(*buffer, *buflen-1);
+  (*buffer)[len]='\0';
+  *buffer += len+1;
+
+  return 0;
+}
 
 enum nss_status _nss_honeypot_getpwnam_r(const char *name, struct passwd *result,
                                       char *buffer, size_t buflen, int *errnop)
 {
 	syslog(LOG_ERR, "_nss_honeypot_getbynam_r() called");
-        mylog( "_nss_honeypot_getpwnam_r() called %s", name );
+        mylog( "_nss_honeypot_getpwnam_r() called %s size:%d", name, buflen );
         //return NSS_STATUS_SUCCESS;
-        return NSS_STATUS_NOTFOUND;
+        size_t remaining_buf = buflen;
+        char* buf = buffer;
+        
+        result->pw_name = buf;
+        append_to_buffer(name,&buf,&remaining_buf);
+        mylog( "   pw_name:%s %p", result->pw_name, result->pw_name );
+
+        result->pw_passwd = buf;
+        append_to_buffer("secreto",&buf,&remaining_buf);
+        mylog( "   pw_passwd:%s %p", result->pw_passwd, result->pw_passwd );
+
+        result->pw_gecos = buf;
+        append_to_buffer("nombrereal",&buf,&remaining_buf);
+        mylog( "   pw_gecos:%s %p", result->pw_gecos, result->pw_gecos );
+
+        result->pw_dir = buf;
+        append_to_buffer("/home",&buf,&remaining_buf);
+        mylog( "   pw_dir:%s %p", result->pw_dir, result->pw_dir );
+
+        result->pw_shell = buf;
+        append_to_buffer("/bin/false",&buf,&remaining_buf);
+        mylog( "   pw_shell:%s %p", result->pw_shell, result->pw_shell );
+
+
+        return NSS_STATUS_SUCCESS;
 } 
